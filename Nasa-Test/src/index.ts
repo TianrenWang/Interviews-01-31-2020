219b2fdc8b29ef2bb224ea6465e69424223721cf
TianrenWang <broodstar_frank@hotmail.com>
2020-01-31 11:15:52 -0500

    First Commit

--- Nasa-Test/src/index.ts
+++ Nasa-Test/src/index.ts
@@ -20,6 +20,29 @@ app.get( '/daily', async ( request: any, response: any ) => {
     response.send(result);
 } );
 
+// Handle get requests to /nasa with dates parameters
+app.get( '/dates', async ( request: any, response: any ) => {
+
+    //Get the requested dates in chronological order
+    var dates = request.query.dates;
+    dates = dates.substring(1, dates.length-1);
+    var dates_list = dates.split(',');
+    dates_list.sort();
+    dates_list = dates_list.reverse();
+
+    // Loop through every date and get the results for each date
+    var results = [];
+
+    for (let i = 0; i < dates_list.length; i++) {
+        const daily = new DailyImage();
+        const result = await daily.getImageForDate(dates_list[i]);
+        results.push(result)
+    }
+
+    // Sends back the result of the image getter
+    response.send({timeline: results});
+} );
+
 // start the Express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
